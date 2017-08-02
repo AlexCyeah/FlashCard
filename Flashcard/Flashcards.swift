b86bd7f95a83546fdc9cb98c9a417cec4e0262f7
iD Student <idstudent@iDs-iMac.local>
2017-08-02 15:52:17 -0700

    added more files

--- Flashcard/Flashcards.swift
+++ Flashcard/Flashcards.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+
+class Flashcard {
+    var question: String
+    
+    var options: [String]
+    var correctAnswer: String
+    
+    init( question: String, options : [String] ) {
+    self.question = question
+    self.options = options
+    
+    self.correctAnswer = options[0]
+    }
+}
+
+class CardCollection {
+    
+    public static var instance: CardCollection = CardCollection()
+
+    public private(set) var cards: [Flashcard]
+    
+    public private(set) var currentIndex: Int
+    
+    public var currentCard: Flashcard {
+        get { return cards[currentIndex] }
+    }
+    
+    private init() {
+        
+        cards = [
+                Flashcard(question: "Food?", options: ["Yes", "No"]),
+                Flashcard(question: "Food Machine Broke?", options: ["Understandable", "Damn."]),
+                Flashcard(question: "Can I get uhh B O N E L E S S pizza?", options: ["What?", "Sure"])]
+        
+        currentIndex = 0
+        
+        
+    }
+    
+    public func nextQuestion() {
+        currentIndex += 1
+        if(currentIndex >= cards.count){
+            currentIndex = 0
+        }
+    }
+    
+    public func checkAnswer(_ selectedAnswer: Int) -> Bool {
+        return currentCard.options[selectedAnswer] == currentCard.correctAnswer
+    }
+}
+
--- Flashcard/ViewController.swift
+++ Flashcard/ViewController.swift
@@ -8,10 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var questionTextView: UITextView!
+    
+    @IBOutlet weak var answerPickerView: UIPickerView!
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        answerPickerView.dataSource = self
+        answerPickerView.dataSource = self
+        
+        setupCardUI()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +32,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func setupCardUI() {
+        questionTextView.text = CardCollection.instance.currentCard.question
+        
+        questionLabel.text = "Question \(CardCollection.instance.currentIndex + 1) /\(CardCollection.instance.cards.count)"
+    }
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    // returns the # of rows in each component..
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return CardCollection.instance.currentCard.options.count
+    }
+    
+    // Pickerview delegates
+    // returns text of option at a given row.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return CardCollection.instance.currentCard.options[row]
+    }
+    
 
 }
 
